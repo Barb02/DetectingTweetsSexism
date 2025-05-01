feb5af55483254accc23e6f1c9c029f7face50ae
Barbara Galiza <barnobregali02@gmail.com>
2025-05-01 20:24:28 +0100

    started task1, done until dfm

---
+++ project.R
@@ -0,0 +1,61 @@
+library(quanteda)
+library(quanteda.textmodels)
+library(quanteda.textplots)
+library(quanteda.textstats)
+library(readr)
+library(dplyr)
+library(recommenderlab)
+library(syuzhet)
+library(textclean)
+
+
+df = read_csv("/home/barbara/MDS/ATDS/DetectingTweetsSexism/tables/EXIST2025_train.csv")
+
+str(df)
+dim(df)
+summary(df)
+
+
+tweets = unique(df$tweet)
+tweets
+length(tweets)
+
+
+tweets <- replace_html(tweets) # expose apersands (&) to be removed later
+tweets <- gsub("@\\w+", "", tweets) # remove usernames
+tweets <- gsub("http\\S+", "", tweets) # remove links
+tweets <- gsub("\\.(\\S)", ". \\1", tweets, perl = TRUE)
+
+# ----------------------------------- NLP -----------------------------------
+
+
+corpus <- corpus(tweets)
+toks <- tokens(
+  corpus,
+  remove_punct = TRUE,
+  remove_symbols = TRUE
+)
+toks <- tokens_tolower(toks)
+toks <- tokens_remove(toks, stopwords("english"))
+toks <- tokens_remove(toks, pattern = "^\\d+$", valuetype = "regex")
+toks <- tokens_wordstem(toks)
+
+dfm <- dfm(toks)
+
+ndoc(dfm)
+nfeat(dfm)
+featnames(dfm)
+textplot_wordcloud(dfm, min_count = 50) 
+
+
+any(grepl("&amp;", featnames(dfm)))  # & removed
+any(grepl("peglulu2", featnames(dfm)))
+any(grepl("zdvaqvaxaj", featnames(dfm)))
+"20" %in% featnames(dfm)
+"1.01" %in% featnames(dfm)
+
+# TF-IDF - Helps downweight common words that occur in many documents while highlighting discriminative words
+dfm_tfidf <- dfm_tfidf(dfm)
+dfm_tfidf
+
+# sentiment analysis: deal with #
